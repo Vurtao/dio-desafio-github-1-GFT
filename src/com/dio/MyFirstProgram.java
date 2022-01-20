85d601f1cc7be395b6565aa6b0cda391b66cf7d8
Vurtao <milanmiguel@gmail.com>
2022-01-20 14:34:32 -0300

    Inclusão do javadoc e alteração no código

--- src/com/dio/MyFirstProgram.java
+++ src/com/dio/MyFirstProgram.java
@@ -2,10 +2,12 @@
 
     import com.dio.base.Order;
 
+    import java.math.BigDecimal;
+
     public class MyFirstProgram {
 
         public static void main(String[] args) {
-            Order order = new Order("code1234");
+            final Order order = new Order( "code1234", new BigDecimal("99.99"));
             System.out.println(order);
         }
     }
--- src/com/dio/base/Order.java
+++ src/com/dio/base/Order.java
@@ -1,17 +1,43 @@
 package com.dio.base;
 
-public class Order {
+import java.math.BigDecimal;
 
-    private final String code;
+    /**
+    * @author Miguel Fuentes
+    * @version 1.0.0
+    * @see BigDecimal
+    * @since Release1.0.0
+    */
+    public class Order {
 
-    public Order(String code) {
+        // representa código do pedido
+        private final String code;
+        private final BigDecimal totalValue;
+
+    /**
+     * Construtor da Classe
+     *
+     * @param code          código do pedido
+     * @param totalValue    valor total do pedido
+     */
+    public Order(String code, BigDecimal totalValue) {
         this.code = code;
+        this.totalValue = totalValue;
     }
 
-    @Override
-    public String toString() {
-        return "Order={" +
-                "code='" + code + "'" +
-                "}";
+    /**
+     *  Calcula o valor total de acordo com o valor do pedido. Se o valor for maior que R$100,00 uma taxa sera cobrada.
+     * @return                      Valor total do pedido com taxas
+     * @throws RuntimeException     Se o valor de pedido for negativo
+     */
+    public BigDecimal calculateFee() throws RuntimeException {
+        if (This.totalValue.signum() < 0) {
+            throw new RuntimeException("O pedido nao pode ter valor negativo");
+        }
+        if (this.totalValue.compareTo(new BigDecimal("100.00")) > 100) {
+            return this.totalValue.multiply(new BigDecimal("0.99"));
+        }
+        return this.totalValue;
     }
+
 }
